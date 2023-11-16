f6dcba4cdd566fc4703653c790c9c3dc390d6212
zziri <jihzang@gmail.com>
2023-11-17 02:33:48 +0900

    build error fix

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,6 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+// @ts-ignore
+const Image = ({ ...rest }: ImageProps) => <img {...rest} />
 
 export default Image
